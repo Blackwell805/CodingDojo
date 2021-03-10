d267a27b4a721b8260b90aa8b44409067f33021b
Austin Blackwell <austinbobs@gmail.com>
2021-03-10 12:44:03 -0800

    Putting it together

--- Mern/react/prop_it_up/src/components/PersonCard.js
+++ Mern/react/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,24 @@
 import React, { Component} from "react";
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props); 
+            this.state = {
+                age: props.age,
+                show: true 
+        };
+    }
+    IncrementItem = () => {
+        this.setState({ age: this.state.age + 1 });
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
         return (
             <div>
                 <h3>{lastName}, {firstName}</h3>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.IncrementItem}>Birthday Button for {firstName} {lastName}</button>
             </div>
         );
     }
