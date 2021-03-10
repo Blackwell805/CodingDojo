f7cc2caabd2741902e8647a93a4d94e870a38925
Austin Blackwell <austinbobs@gmail.com>
2021-03-09 17:16:21 -0800

    First JSX webpage

---
+++ Mern/react/myfirstreact/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h3>Things I need to Learn:</h3>
+      <ul>
+        <li>Learn React!</li>
+        <li>Play the Ukelele!</li>
+        <li>Speak Spanish!</li>
+        <li>Run half marathon!</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
