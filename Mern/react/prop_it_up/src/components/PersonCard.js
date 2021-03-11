57e7e154578782548d68ef7c69edd56c4b93b8eb
Austin Blackwell <austinbobs@gmail.com>
2021-03-10 17:51:09 -0800

    Big Inversion Assignment added

--- Mern/react/prop_it_up/src/components/PersonCard.js
+++ Mern/react/prop_it_up/src/components/PersonCard.js
@@ -1,27 +1,13 @@
-import React, { Component} from "react";
+import React from "react";
 
-class PersonCard extends Component{
-    constructor(props){
-        super(props); 
-            this.state = {
-                age: props.age,
-                show: true 
-        };
-    }
-    IncrementItem = () => {
-        this.setState({ age: this.state.age + 1 });
-    }
-    render(){
-        const {firstName, lastName, age, hairColor} = this.props;
-        return (
-            <div>
-                <h3>{lastName}, {firstName}</h3>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button onClick={this.IncrementItem}>Birthday Button for {firstName} {lastName}</button>
-            </div>
-        );
-    }
+
+const PersonCard = props => {
+    return (
+        <div>
+        <h3>{props.lastName}, {props.firstName}</h3>
+        <p>Age: {props.age}</p>
+        <p>Hair Color: {props.hairColor}</p>
+        </div>
+    );
 }
-
 export default PersonCard;
