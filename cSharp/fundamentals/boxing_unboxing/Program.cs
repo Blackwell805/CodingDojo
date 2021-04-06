ed8ef98031d41add31b8861d8bf1dd1e25231fd1
Austin Blackwell <austinbobs@gmail.com>
2021-04-05 22:08:53 -0700

    Boxing/Unboxing

---
+++ cSharp/fundamentals/boxing_unboxing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> random = new List<object>();
+            random.Add(7);
+            random.Add(28);
+            random.Add(-1);
+            random.Add(true);
+            random.Add("chair");
+
+            foreach (object thing in random)
+            {
+                Console.WriteLine(thing);
+            }
+
+            int sum = 0;
+            for (int i = 0; i < random.Count; i++)
+            {
+                if (random[i] is int)
+                {
+                    sum += (int)random[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
